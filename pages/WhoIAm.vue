fe85266acaa03e0ea3300164c79f99b10fa5c301
Lucas Gehlen <contato.lucasgehlen@gmail.com>
2024-08-31 21:34:53 -0300

    Change Lucas Image

--- pages/WhoIAm.vue
+++ pages/WhoIAm.vue
@@ -3,7 +3,7 @@
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div class="grid grid-cols-2 gap-4 mt-8 max-w-lg">
                 <img class="max-w-full max-h-fit rounded-lg" src="~/assets/images/lucas1.webp" alt="Foto 1 de Lucas Gehlen" loading="lazy">
-                <img class="mt-4 max-w-full max-h-fit lg:mt-10 rounded-lg" src="~/assets/images/lucas2.webp" alt="Foto 2 de Lucas Gehlen" loading="lazy">
+                <img class="mt-4 max-w-full max-h-fit lg:mt-10 rounded-lg" src="~/assets/images/lucas3.png" alt="Foto 2 de Lucas Gehlen" loading="lazy">
             </div>
             <div class="font-light sm:text-lg text-gray-400">
                 <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-white">{{ t('whoAmI') }}</h2>
